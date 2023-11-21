97997957bbf735654ab1f35832c4a0343d17a30a
jritsqmet <121739152+jritsqmet@users.noreply.github.com>
2023-11-21 13:44:00 -0500

    Update BibliotecaApplication.java

--- src/main/java/com/itsqmet/Biblioteca/BibliotecaApplication.java
+++ src/main/java/com/itsqmet/Biblioteca/BibliotecaApplication.java
@@ -10,4 +10,17 @@ public class BibliotecaApplication {
 		SpringApplication.run(BibliotecaApplication.class, args);
 	}
 
+	/*
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*").allowedHeaders("*");
+			}
+		};
+	}
+	
+	 */
+
 }
